55eeda71579ea6838c48fae4f6f7e1e2a63b82b7
Charles Varley <2391564V@student.gla.ac.uk>
2019-02-28 10:18:53 +0000

    Index Page

--- reviews/urls.py
+++ reviews/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import url
 from reviews import views
 
 urlpatterns = [
+    # Development URL to fill in for pages yet to be created
+    url(r'^$', views.index, name='#'),
+    # URLs
     url(r'^$', views.index, name='index'),
 ]
