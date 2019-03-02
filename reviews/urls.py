c56f76da074f6370f6998fcfac6fa3da8c7b19b8
Ian Ballingall <2319170B@student.gla.ac.uk>
2019-03-02 16:17:06 +0000

    Fix URL regex

--- reviews/urls.py
+++ reviews/urls.py
@@ -6,14 +6,14 @@ urlpatterns = [
     url(r'^$', views.index, name='#'),
     # URLs
     url(r'^$', views.index, name='index'),
-    url(r'about/$', views.about, name='about'),
-    url(r'games/$', views.games, name='games'),
-    url(r'game/$', views.game, name='game'),
-    url(r'profile/$', views.profile, name='profile'),
-    url(r'edit/$', views.edit, name='edit'),
-    url(r'register/$', views.register, name='register'),
-    url(r'login/$', views.login, name='login'),
-    url(r'restricted/$', views.restricted, name='restricted'),
-    url(r'review/$', views.review, name='review'),
+    url(r'^about/$', views.about, name='about'),
+    url(r'^games/$', views.games, name='games'),
+    url(r'^game/$', views.game, name='game'),
+    url(r'^profile/$', views.profile, name='profile'),
+    url(r'^edit/$', views.edit, name='edit'),
+    url(r'^register/$', views.register, name='register'),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^restricted/$', views.restricted, name='restricted'),
+    url(r'^review/$', views.review, name='review'),
 
 ]
