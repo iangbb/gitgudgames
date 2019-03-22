32383d2f0aa8b92be9ee2d5af6ac08c245f21f0e
Ian Ballingall <2319170B@student.gla.ac.uk>
2019-03-22 11:47:42 +0000

    JS commentary

--- static/js/game.js
+++ static/js/game.js
@@ -2,27 +2,35 @@ let reviewNo = 0;
 let commentNo = [];
 var noMoreReviews = 0;
 
+// Called when page is loaded
 $(document).ready(function() {
 
+// Create 'get reviews' button and define functions
 $('.get-reviews').click(function() {
-    const authenticated = ($(this).attr("data-authenticated") == 'true');
-    const gameid = $(this).attr("data-gameid");
+    const authenticated = ($(this).attr("data-authenticated") == 'true'); // Is user logged in?
+    const gameid = $(this).attr("data-gameid"); // Get the game's unique ID
+	// AJAX request to get reviews
     $.getJSON('/ajax/get_reviews/', {
         game: gameid,
         start: reviewNo,
     }, function(data, textStatus, jqXHR) {
-        reviewNo = reviewNo + 3;
-        noMoreReviews = data['number'] - 3
-        $('.get-reviews').html('Get more reviews (' + noMoreReviews + ')')
+        reviewNo = reviewNo + 3; // Track what the start index of the next 3 reviews it
+        noMoreReviews = data['number'] - 3; // Track how many more reviews are left to get
+        $('.get-reviews').html('Get more reviews (' + noMoreReviews + ')');
         const parser = new DOMParser();
+		
+		// For each review returned by AJAX, generate the HTML
         for (i = 0; i < data.reviews.length; i++) {
             commentNo[data.reviews[i].id] = 0;
             var className = "jumbotron"
             var journalString = "";
+			
+			// If the poster is a journalist, then highlight their review
             if (data.reviews[i].is_journalist) {
                 journalString = "[ This is a journalist review ]"
                 className = "jumbotron journalist-review"
             }
+			
             const domString = '<div class="' + className +'">\
                                 <h5> ' + journalString + ' </h5>\
                               <div class="row">\
@@ -71,34 +79,44 @@ $('.get-reviews').click(function() {
 
             $('.journal')
 
+			// Hide 'add comment' form if user is not authenticated
             if (authenticated == false){
                 console.log("here");
                 $('.add-comment').hide();
                 $(".form-control").hide();
 
             }
+			
+			// If there are no more reviews to retrieve, hide the 'get more' button
             if (data.more == false) {
                 $('.getmorereviews').hide();
 
             }
+			
+			// Generate the visual star ratings on the reviews
             const starwidth = (data.reviews[i].rating * 20 + "%");
             $("#" + data.reviews[i].id + "").width(starwidth);
         }
-        $(".get-comments").trigger("click");
+        $(".get-comments").trigger("click"); // Retrieve first 3 comments
     });
 });
 $(".get-reviews").trigger("click");
 
+// Create 'get comments' button, which is specific to each review
 $(document).on("click", ".get-comments" , function() {
         const reviewid = $(this).attr("data-reviewid");
+			// AJAX request to get comments
             $.getJSON('/ajax/get_comments/', {
                 review : reviewid,
                 start: commentNo[reviewid],
             }, function(data, textStatus, jqXHR) {
+				// Track starting index of next 3 comments and the number remaining for each review
                 commentNo[reviewid] = commentNo[reviewid] + 3;
                 noMoreComments = data['number'] - 3
                 $('.get-comments.review' + reviewid).html('Get more comments (' + noMoreComments + ')')
                 const parser = new DOMParser();
+				
+				// For each comment, generate HTML
                 for (i = 0; i < data.comments.length; i++) {
                     if (!($('.' + data.comments[i].id).length)){
                     const domString = '<div class="jumbotron comments ' + data.comments[i].id +'">\
@@ -118,26 +136,33 @@ $(document).on("click", ".get-comments" , function() {
 
                     }
                 }
-                    if (data.more == false) {
-                        $('.get-comments.review' + reviewid).hide();
+				
+				// If there are no more comments, hide the 'get comments' button
+                if (data.more == false) {
+                    $('.get-comments.review' + reviewid).hide();
                 }
         });
         });
 
+// Define 'add comment' button
 $(document).on("click", ".add-comment" , function() {
     const reviewid = $(this).attr("data-reviewid");
     const commentText = $("#addcomment" + reviewid).val();
+	
+	// AJAX request structure for POST request
     $.ajax({
         url : '/ajax/add_comment/',
         type : 'POST',
         data : { review : reviewid, comment_text : commentText},
 
+		// Define function which is called on a successful comment submission
         success : function(data) {
+			// Hide the add comment box & button and error text
             $('.add-comment').hide();
             $("#addcomment" + reviewid).hide();
             $('.error-' + reviewid).hide();
 
-
+			// Display the newly created comment from AJAX response
             const parser = new DOMParser();
             const domString = '<div class="jumbotron comments ' + data.comment.id +'">\
                               <div class="row">\
@@ -158,8 +183,9 @@ $(document).on("click", ".add-comment" , function() {
 
         },
 
+		// Define function called when comment posting fails
         error : function(xhr, errmsg, err){
-            $('.error-' + reviewid).html(JSON.parse(xhr.responseText).error);
+            $('.error-' + reviewid).html(JSON.parse(xhr.responseText).error); // Display error returned by the server
         }
 
 });
@@ -167,6 +193,9 @@ $(document).on("click", ".add-comment" , function() {
 });
 });
 
+// This function adds the CSRF token to the POST request, so that adding comments
+// works correctly whilst retaining the security function provided by CSRF tokens.
+// This code originates from https://github.com/realpython/django-form-fun/blob/master/part1/main.js
 $(function() {
 
 
